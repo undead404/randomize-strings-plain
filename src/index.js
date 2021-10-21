2bcd48292a3f8ce7a671a362efd48e7918bcf00e
Vitalii Perehonchuk <vitalii_perehonchuk@epam.com>
2021-10-21 20:45:56 +0300

    modules?

--- src/index.js
+++ src/index.js
@@ -1,32 +1,8 @@
+import shuffle from "./utils/shuffle";
+
 // Cache elements not to search for them every time
 const $button = document.getElementById("shuffle-button");
 const $textarea = document.getElementById("strings-input");
-
-/**
- * Returns new array with shuffled items
- * @param {Array} array
- * @returns {Array} shuffled array
- */
-function shuffle(array) {
-  const shuffledArray = [...array];
-  let currentIndex = array.length,
-    randomIndex;
-
-  // While there remain elements to shuffle...
-  while (currentIndex != 0) {
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-
-    // And swap it with the current element.
-    [shuffledArray[currentIndex], shuffledArray[randomIndex]] = [
-      shuffledArray[randomIndex],
-      shuffledArray[currentIndex],
-    ];
-  }
-
-  return shuffledArray;
-}
 
 $button.addEventListener("click", () => {
   try {
